acfa69f1feed7ad8a42382e51b2ed40540ef27ee
David Andersson <david.andersson@zocom.se>
2023-11-01 11:05:33 +0100

    2.8

---
+++ 2.8.js
@@ -0,0 +1,18 @@
+// ange variabel: summa
+// upprepning: 1 till och med 10
+// - lägga ihop tal
+// skriv ut: summa
+
+let summa = 0
+let count = 1
+while( count <= 10 ) {
+	summa = summa + count
+	count = count + 1
+}
+console.log('Summan är ', summa);
+
+// 1+2+3+4+5
+// 3 + 3+4+5
+// 6 +   4+5
+// 10 +    5
+// 15
